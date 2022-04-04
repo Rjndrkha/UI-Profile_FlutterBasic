fda0e15d9cb8aa94c909271996ab8f4a94e7d1b3
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2022-04-04 14:00:21 +0700

    DONE ALL

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_basic/theme.dart';
 
@@ -10,53 +12,76 @@ class HomePage extends StatelessWidget {
       backgroundColor: background,
       body: SafeArea(
         child: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 50,
-              ),
-              Text(
-                'Home Page',
-                style: TextStyle(
-                  color: primary,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w600,
+          child: SingleChildScrollView(
+            //agar bisa di scrooling
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 30,
                 ),
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Image.asset(
-                'assets/images/primary.png',
-                width: 140,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Rajendra Rakha',
-                style: TextStyle(
-                  color: primary,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
+                Text(
+                  'Home Page',
+                  style: TextStyle(
+                    color: primary,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w600,
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 4,
-              ),
-              Text(
-                'Flutter Developer',
-                style: TextStyle(
-                  color: grey,
-                  fontSize: 15,
-                  fontWeight: FontWeight.w500,
+                SizedBox(
+                  height: 20,
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Center(
-                child: Row(
+                Image.asset(
+                  'assets/images/primary.png',
+                  width: 140,
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Rajendra Rakha',
+                  style: TextStyle(
+                    color: primary,
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Flutter Developer',
+                  style: TextStyle(
+                    color: grey,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Image.asset(
+                        'assets/images/1.png',
+                        width: 80,
+                      ),
+                      Image.asset(
+                        'assets/images/2.png',
+                        width: 80,
+                      ),
+                      Image.asset(
+                        'assets/images/1.png',
+                        width: 80,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Image.asset(
@@ -73,28 +98,85 @@ class HomePage extends StatelessWidget {
                     ),
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Image.asset(
-                    'assets/images/1.png',
-                    width: 80,
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  width: 224,
+                  height: 55,
+                  child: RaisedButton(
+                    onPressed: () {
+                      showModalBottomSheet(
+                          context: context,
+                          builder: (context) {
+                            return Container(
+                              height: 280,
+                              color: white,
+                              padding: EdgeInsets.symmetric(vertical: 50),
+                              child: Column(children: [
+                                Center(
+                                  child: Text(
+                                    'Update Photo',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 22),
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Center(
+                                  child: Text(
+                                    'You are only able to change\nthe picture profile once',
+                                    style: TextStyle(
+                                      color: grey,
+                                      fontSize: 15,
+                                    ),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Container(
+                                  width: 225,
+                                  height: 55,
+                                  child: RaisedButton(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(16)),
+                                    color: button,
+                                    onPressed: () {},
+                                    child: Text(
+                                      'Continue',
+                                      style: TextStyle(
+                                          color: white,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ),
+                                )
+                              ]),
+                            );
+                          });
+                    },
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16)),
+                    color: button,
+                    child: Text(
+                      'Update Profile',
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   ),
-                  Image.asset(
-                    'assets/images/2.png',
-                    width: 80,
-                  ),
-                  Image.asset(
-                    'assets/images/1.png',
-                    width: 80,
-                  ),
-                ],
-              )
-            ],
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/theme.dart
+++ lib/theme.dart
@@ -4,3 +4,4 @@ Color primary = Color(0xff314728);
 Color grey = Color(0xffA9B0A6);
 Color button = Color(0xffF9B650);
 Color background = Color(0xffE5E5E5);
+Color white = Color(0xffFFFFFF);
